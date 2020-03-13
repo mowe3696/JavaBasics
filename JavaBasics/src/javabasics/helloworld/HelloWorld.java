fc3dc6b4d73a6118f6ce68d715f977d0a67c2f83
mowe3696 <36576597+mowe3696@users.noreply.github.com>
2020-03-13 09:45:01 +0100

    git update

--- JavaBasics/src/javabasics/helloworld/HelloWorld.java
+++ JavaBasics/src/javabasics/helloworld/HelloWorld.java
@@ -1,7 +1,8 @@
 package javabasics.helloworld;
 
-public class HelllWorld {
-
+public class HelloWorld {
+	
+	// checking for git update
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
