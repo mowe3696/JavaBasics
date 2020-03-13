776c27056b30322a8b751e5ddafd024c53ad7185
mowe3696 <36576597+mowe3696@users.noreply.github.com>
2020-03-13 11:19:43 +0100

    to copy

--- JavaBasics/src/javabasics/helloworld/HelloWorld.java
+++ JavaBasics/src/javabasics/helloworld/HelloWorld.java
@@ -3,8 +3,8 @@ package javabasics.helloworld;
 public class HelloWorld {
 	
 	// checking for GIT update2
-	// new comment
-	// second comment
+	// TEST comment
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
